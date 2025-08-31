be0f791fa973558b7fbf08f20773bf3e694afb12
Cauê Souza <caue.souzapl@gmail.com>
2025-08-31 02:21:30 -0300

    Fix formatting of JSON import assertion

--- results-summary-component-main/script/index.js
+++ results-summary-component-main/script/index.js
@@ -1,4 +1,4 @@
-import data from '../data.json' assert{type: "json"};
+import data from '../data.json' assert {type: "json"};
 const dataElement = document.querySelector('#data');
 const scoreElement = document.querySelector('.highlight')
 
