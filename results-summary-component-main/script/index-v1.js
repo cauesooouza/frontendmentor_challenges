38c90e3861937979460d31bebc7f4d2924e07d76
Cauê Souza <caue.souzapl@gmail.com>
2025-08-31 03:14:38 -0300

    fix

--- results-summary-component-main/script/index-v1.js
+++ results-summary-component-main/script/index-v1.js
@@ -1,4 +1,26 @@
-const data = require('../data.json');
+const data = [
+  {
+    "category": "Reaction",
+    "score": 80,
+    "icon": "./assets/images/icon-reaction.svg"
+  },
+  {
+    "category": "Memory",
+    "score": 92,
+    "icon": "./assets/images/icon-memory.svg"
+  },
+  {
+    "category": "Verbal",
+    "score": 61,
+    "icon": "./assets/images/icon-verbal.svg"
+  },
+  {
+    "category": "Visual",
+    "score": 72,
+    "icon": "./assets/images/icon-visual.svg"
+  }
+]
+
 const dataElement = document.querySelector('#data');
 const scoreElement = document.querySelector('.highlight')
 
